5fdd23f2aab7f8af941277ea496c3ab98e3f7024
Edward Korkollie <korkollieed@gmail.com>
2019-07-08 16:48:31 -0400

    update

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,106 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import javax.management.ListenerNotFoundException;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 
 public class GroceryReporter {
+
     private final String originalFileText;
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
+
+public String bread() {
+        return null;
+}
+
     @Override
     public String toString() {
+        String groceryList = "";
+        ItemParser parser = new ItemParser();
+        List<Item> items = parser.parseItemList(originalFileText);
+        Map<String, List<Double>> freq = mapFrequencies(items);
+        for(Map.Entry<String, List<Double>> entry : freq.entrySet()) {
+            groceryList += formatItem(entry.getKey(), entry.getValue());
+
+        }
         return null;
+    }
+
+
+    public String capString(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    public String formatItem(String itemName, List<Double> freq) {
+        String format = "";
+        format += String.format("name:%8s \t\t seen:%2d %s", capString(itemName),
+        freq.size(), times(freq.size())) + '\n' + "============ \t\t =============\n";
+        return format;
+    }
+
+    public String formatPrice(List<Double> price) {
+        return String.format("Price:%7.2f \t\t seen:%2d %s", price.get(0), price.size(),times(price.size()))
+                + '\n' + "------------- \t\t -------------\n";
+    }
+
+//    public String formatMorePrices(List<Double> price) {
+//        String format = "";
+//        int counter =0;
+//        Map<Double, Integer> priceAppearance = mapPricefreq(price);
+//        for(Map.Entry<Double, Integer> entry: priceAppearance.entrySet()) {
+//
+//            counter++;
+//            format += String.format("Price:%7.2f \t\t seen:2d %s\n", entry.getKey(), entry.getValue(), times(entry.getValue()));
+//            if(counter != priceAppearance.entrySet().size()) {
+//                format += "------------- \t\t -------------\n";
+//            }
+//        }
+//        return format;
+//    }
+
+//    public Map<Double, Integer> mapPricefreq(List<Double> prices) {
+//        Map<Double, Integer> priceAppearance = new LinkedHashMap<>();
+//        for(Double price : prices) {
+//            if(priceAppearance.containsKey(price)) {
+//                Integer freq = priceAppearance.get(price);
+//                priceAppearance.put(price, freq + 1);
+//            }
+//            else {
+//                priceAppearance.put(price, 1);
+//            }
+//        }
+//        return priceAppearance;
+//    }
+
+    public String times(int j) {
+        if(j == 1) {
+            return "time";
+        }
+        else{
+            return "time";
+        }
+    }
+
+    public Map<String, List<Double>> mapFrequencies(List<Item> items) {
+        Map<String, List<Double>> freq = new LinkedHashMap<>();
+
+        for(Item item: items) {
+            String itemName = item.getName();
+            if(freq.containsKey(itemName)) {
+                List<Double> prices = freq.get(itemName);
+                prices.add(item.getPrice());
+                freq.put(itemName, prices);
+            }
+        }
+        return freq;
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,57 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
+    private int exceptionCount;
+    public ItemParser() {
+
+        this.exceptionCount=0;
+    }
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        String[] array = valueToParse.split("##");
+        List<Item> items = new ArrayList<>();
+
+        for(int i=0; i<array.length; i++) {
+            try{
+                items.add(parseSingleItem(array[i]));
+            }catch (Exception e) {
+                exceptionCount++;
+                System.out.println("Item list was not added to list (not valid).");
+            }
+        }
+
+        return items;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        String pattern ="(\\w+) [*:,@^%!](\\w+)[*:,@^%!](\\w+)[*:,@^%!](\\d.{3,4})[*:,@^%!](\\w+)[*:,@^%!](\\w+)" +
+                "[*:,@^%!](\\w+)[*:,@^%!](\\d{1,2}/\\d{1,2}/\\d{4}).*";
+        Pattern pattern1 = Pattern.compile(pattern);
+        Matcher matcher = pattern1.matcher(singleItem);
+
+        try{
+            matcher.matches();
+            String name = matcher.group(2).toLowerCase();
+            if(name.contains("0")) {
+                name = name.replace('0', 'o');
+            }
+            Double price = Double.valueOf(matcher.group(4));
+            String string = matcher.group(6).toLowerCase();
+            String expireDate = matcher.group(8);
+            return new Item(name, price, string,expireDate);
+
+        }catch (Exception e) {
+            throw new ItemParseException();
+        }
+
+    }
+    public String converzeroTOO(String str) {
+        return str.replace('0', 'o');
     }
 }
